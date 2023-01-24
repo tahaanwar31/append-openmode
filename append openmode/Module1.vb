67dbc145b29aa4d9d9ada73ad9fe4456177dbab6
Taha Anwar <123452083+tahaanwar31@users.noreply.github.com>
2023-01-24 15:52:24 +0500

    First Commit

---
+++ append openmode/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        FileOpen(1, My.Application.Info.DirectoryPath & "\HighAchieversOfBSS.txt", OpenMode.Append)
+        For x = 1 To 5
+            Console.WriteLine("Enter Name: ")
+            name = Console.ReadLine()
+            WriteLine(1, name)
+        Next
+        FileClose()
+    End Sub
+
+End Module
